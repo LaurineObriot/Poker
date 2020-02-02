08f1ccb1d07fc4dfc53a348b45bcb59410ad0c60
Lauobrio42 <laurine.obriotbouvier@outlook.fr>
2020-02-02 19:58:24 +0100

    emits the event

--- poker_modules/deck.js
+++ poker_modules/deck.js
@@ -1,46 +1,92 @@
+ar Deck = require('./deck'),
+	Pot = require('./pot');
+
 /**
-* The deck "class"
-*/
-
-var Deck = function() {
-	this.next_card = 0;
-	this.cards = [
-		'As', 'Ah', 'Ad', 'Ac',
-		'Ks', 'Kh', 'Kd', 'Kc',
-		'Qs', 'Qh', 'Qd', 'Qc',
-		'Js', 'Jh', 'Jd', 'Jc',
-		'Ts', 'Th', 'Td', 'Tc',
-		'9s', '9h', '9d', '9c',
-		'8s', '8h', '8d', '8c',
-		'7s', '7h', '7d', '7c',
-		'6s', '6h', '6d', '6c',
-		'5s', '5h', '5d', '5c',
-		'4s', '4h', '4d', '4c',
-		'3s', '3h', '3d', '3c',
-		'2s', '2h', '2d', '2c',
-	];
-
-
-// Method that sufflues the deck
-Deck.prototype.suffle = function () {
-	this.next_card = 0;
-	var suffled_deck = [];
-
-	for (var i = 0; i < 52; i++) {
-		var random_card = this.cards.splice( Math.floor( Math.random() * this.cards.length ), 1 );
-		shuffled_deck = shuffled_deck.concat( random_card );
-			}
-	this.cards = suffled_deck;
-}
-
-// Method that returns the next x cards of the deck
-Deck.prototype.deal = function(number_of_cards) {
-	var dealt_cards = [];
-	for (var i = 0; i < number_of_cards && this.next_card < 52; i++) {
-		dealt_cards.push(this.cards[this.next_cards]);
-		this.next_card++;
+ * The table "class"
+ * @param string	id (the table id)
+ * @param string	name (the name of the table)
+ * @param object 	deck (the deck object that the table will use)
+ * @param function 	eventEmitter (function that emits the events to the players of the room)
+ * @param int 		seatsCount (the total number of players that can play on the table)
+ * @param int 		bigBlind (the current big blind)
+ * @param int 		smallBlind (the current smallBlind)
+ * @param int 		maxBuyIn (the maximum amount of chips that one can bring to the table)
+ * @param int 		minBuyIn (the minimum amount of chips that one can bring to the table)
+ * @param bool 		privateTable (flag that shows whether the table will be shown in the lobby)
+ */
+var Table = function( id, name, eventEmitter, seatsCount, bigBlind, smallBlind, maxBuyIn, minBuyIn, privateTable ) {
+	// The table is not displayed in the lobby
+	this.privateTable = privateTable;
+	// The number of players who receive cards at the begining of each round
+	this.playersSittingInCount = 0;
+	// The number of players that currently hold cards in their hands
+	this.playersInHandCount = 0;
+	// Reference to the last player that will act in the current phase (originally the dealer, unless there are bets in the pot)
+	this.lastPlayerToAct = null;
+	// The game has begun
+	this.gameIsOn = false;
+	// The game has only two players
+	this.headsUp = false;
+	// References to all the player objects in the table, indexed by seat number
+	this.seats = [];
+	// The deck of the table
+	this.deck = new Deck;
+	// The function that emits the events of the table
+	this.eventEmitter = eventEmitter;
+	// The pot with its methods
+	this.pot = new Pot;
+	// All the public table data
+	this.public = {
+		// The table id
+		id: id,
+		// The table name
+		name: name,
+		// The number of the seats of the table
+		seatsCount: seatsCount,
+		// The number of players that are currently seated
+		playersSeatedCount: 0,
+		// The big blind amount
+		bigBlind: bigBlind,
+		// The small blind amount
+		smallBlind: smallBlind,
+		// The minimum allowed buy in
+		minBuyIn: minBuyIn,
+		// The maximum allowed buy in
+		maxBuyIn: maxBuyIn,
+		// The amount of chips that are in the pot
+		pot: this.pot.pots,
+		// The biggest bet of the table in the current phase
+		biggestBet: 0,
+		// The seat of the dealer
+		dealerSeat: null,
+		// The seat of the active player
+		activeSeat: null,
+		// The public data of the players, indexed by their seats
+		seats: [],
+		// The phase of the game ('smallBlind', 'bigBlind', 'preflop'... etc)
+		phase: null,
+		// The cards on the board
+		board: ['', '', '', '', ''],
+		// Log of an action, displayed in the chat
+		log: {
+			message: '',
+			seat: '',
+			action: ''
+		},
+	};
+	// Initializing the empty seats
+	for( var i=0 ; i<this.public.seatsCount ; i++ ) {
+		this.seats[i] = null;
 	}
-	return dealt_cards;
-}
+};
 
-module.exports = Deck;
+// The function that emits the events of the table
+Table.prototype.emitEvent = function( eventName, eventData ){
+	this.eventEmitter( eventName, eventData );
+	this.log({
+		message: '',
+		action: '',
+		seat: '',
+		notification: ''
+	});
+};
