ab2cbe6b184bde5ee2d6c0cef6eb6f23e9bc4dd6
Lauobrio42 <laurine.obriotbouvier@outlook.fr>
2020-02-08 20:42:05 +0100

    creation object pot

---
+++ poker_modules/pot.js
@@ -0,0 +1,16 @@
+/**
+ * The pot object
+ */
+var Pot = function() {
+  // The pot may be split to several amounts, since not all players
+  // have the same money on the table
+  // Each portion of the pot has an amount and an array of the
+  // contributors (players who have betted in the pot and can
+  // win it in the showdown)
+  this.pots = [
+    {
+      amount: 0,
+      contributors: []
+    }
+  ];
+};
