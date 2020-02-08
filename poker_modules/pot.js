7a2348561f67c9fa0e63d1db7db55b6fb8d917aa
Lauobrio42 <laurine.obriotbouvier@outlook.fr>
2020-02-08 20:44:15 +0100

    Prend les bets des joueurs et les ajoutes au pot

--- poker_modules/pot.js
+++ poker_modules/pot.js
@@ -23,3 +23,33 @@ Pot.prototype.reset = function() {
   this.pots[0].amount = 0;
   this.pots[0].contributors = [];
 };
+
+/**
+ * Method that gets the bets of the players and adds them to the pot
+ * @param array players (the array of the tables as it exists in the table)
+ */
+Pot.prototype.addTableBets = function( players ) {
+  // Getting the current pot (the one in which new bets should be added)
+  var currentPot = this.pots.length-1;
+
+  // The smallest bet of the round
+  var smallestBet = 0;
+  // Flag that shows if all the bets have the same amount
+  var allBetsAreEqual = true;
+
+  // Trying to find the smallest bet of the player
+  // and if all the bets are equal
+  for( var i in players ) {
+    if( players[i] && players[i].public.bet ) {
+      if( !smallestBet ) {
+        smallestBet = players[i].public.bet;
+      }
+      else if( players[i].public.bet != smallestBet ) {
+        allBetsAreEqual = false;
+
+        if( players[i].public.bet < smallestBet ) {
+          smallestBet = players[i].public.bet;
+        }
+      }
+    }
+  }
