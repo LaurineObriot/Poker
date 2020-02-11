9702c74090946e960fb398b66567b88c99f7353d
Lauobrio42 <laurine.obriotbouvier@outlook.fr>
2020-02-11 21:08:12 +0100

    ajout du player & de la table + eventEmmit

---
+++ spec/preflop.js
@@ -0,0 +1,7 @@
+var Player = require('../poker_modules/player.js');
+var Table = require('../poker_modules/table.js');
+
+var eventEmitter = function( tableId ) {
+	return function ( eventName, eventData ) {
+	}
+}
