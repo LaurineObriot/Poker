86fd1d35f76acb0ecd6739e95b83a24523fce69d
Lauobrio42 <laurine.obriotbouvier@outlook.fr>
2020-02-11 21:08:59 +0100

    la table de tests

--- spec/preflop.js
+++ spec/preflop.js
@@ -11,3 +11,23 @@ var socket = {
 		return;
 	}
 };
+
+function initializeTestTable() {
+	var players = [],
+		table;
+
+		table = new Table( 0, 'Sample 10-handed Table', eventEmitter(0), 10, 2, 1, 200, 40, false );
+
+		for( var i=0 ; i<4 ; i++ ) {
+			players[i] = new Player( socket, 'Player_'+i, 1000 );
+			players[i].socket = socket;
+
+		}
+
+		table.playerSatOnTheTable( players[0], 2, 1000 );
+		table.playerSatOnTheTable( players[1], 6, 1000 );
+		table.playerSatOnTheTable( players[2], 4, 1000 );
+		table.playerSatOnTheTable( players[3], 8, 1000 );
+
+		return table;
+}
