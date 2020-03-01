4f0a15226b97f318e9eb2dfa318958a95cceaf38
Lauobrio42 <laurine.obriotbouvier@outlook.fr>
2020-03-01 19:50:58 +0100

    max bet amount

--- public/js/controllers/table.js
+++ public/js/controllers/table.js
@@ -41,3 +41,8 @@ function( $scope, $rootScope, $http, $routeParams, $timeout, sounds ) {
 			return $scope.table.seats[$scope.mySeat].chipsInPlay < $scope.table.bigBlind ? $scope.table.seats[$scope.mySeat].chipsInPlay : $scope.table.bigBlind;
 		}
 	}
+	
+	$scope.maxBetAmount = function() {
+		if( $scope.mySeat === null || typeof $scope.table.seats[$scope.mySeat] === 'undefined' || $scope.table.seats[$scope.mySeat] === null ) return 0;
+		return $scope.actionState === "actBettedPot" ? $scope.table.seats[$scope.mySeat].chipsInPlay + $scope.table.seats[$scope.mySeat].bet : $scope.table.seats[$scope.mySeat].chipsInPlay;
+	}
