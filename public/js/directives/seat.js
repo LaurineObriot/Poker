37473c39c8427ee75f57621592de6589c6da2576
Lauobrio42 <laurine.obriotbouvier@outlook.fr>
2020-03-11 21:41:02 +0100

    seat occupé ?

--- public/js/directives/seat.js
+++ public/js/directives/seat.js
@@ -20,17 +20,24 @@ app.directive( 'seat', [function() {
 			showBuyInModal: '&'
 		},
 		link: function(scope, element, attributes) {
-		scope.seatIndex = parseInt(attributes.seatIndex);
-		scope.cellNumber = parseInt(attributes.cellNumber);
+			scope.seatIndex = parseInt(attributes.seatIndex);
+			scope.cellNumber = parseInt(attributes.cellNumber);
 
-		scope.getCardClass = function( seat, card ) {
-			if( scope.mySeat === seat ) {
-				return scope.myCards[card];
+			scope.getCardClass = function( seat, card ) {
+				if( scope.mySeat === seat ) {
+					return scope.myCards[card];
+				}
+				else if ( typeof scope.player !== 'undefined' && scope.player && scope.player.cards && scope.player.cards[card] ) {
+					return 'card-' + scope.player.cards[card];
+				}
+				else {
+					return 'card-back';
+				}
 			}
-			else if ( typeof scope.player !== 'undefined' && scope.player && scope.player.cards && scope.player.cards[card] ) {
-				return 'card-' + scope.player.cards[card];
-			}
-			else {
-				return 'card-back';
+
+			scope.seatOccupied = function( seat ) {
+				return !scope.sittingOnTable || ( typeof scope.player !== 'undefinde' && scope.player && scope.player.name );
 			}
 		}
+	};
+}]);
