a95fe243d036eb3ac4f079254d0f18badfa89b98
Lauobrio42 <laurine.obriotbouvier@outlook.fr>
2020-03-11 21:40:16 +0100

    seatIndex & getCardClass

--- public/js/directives/seat.js
+++ public/js/directives/seat.js
@@ -19,3 +19,18 @@ app.directive( 'seat', [function() {
 			notifications: '=',
 			showBuyInModal: '&'
 		},
+		link: function(scope, element, attributes) {
+		scope.seatIndex = parseInt(attributes.seatIndex);
+		scope.cellNumber = parseInt(attributes.cellNumber);
+
+		scope.getCardClass = function( seat, card ) {
+			if( scope.mySeat === seat ) {
+				return scope.myCards[card];
+			}
+			else if ( typeof scope.player !== 'undefined' && scope.player && scope.player.cards && scope.player.cards[card] ) {
+				return 'card-' + scope.player.cards[card];
+			}
+			else {
+				return 'card-back';
+			}
+		}
